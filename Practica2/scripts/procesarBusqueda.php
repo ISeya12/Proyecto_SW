3f1c487777696eaf9991b78e5542e6ce3cfbd9dc
AlonsoCampi09 <alocam01@ucm.es>
2024-03-08 22:14:19 +0100

    Revert "Script añadido que realiza la busqueda segun"
    This reverts commit a7fb7efb5303519fe822b812a6d1b7d38551ff18.

--- Practica2/scripts/procesarBusqueda.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $nombre_a_buscar= filter_input(INPUT_POST, 'contenido', FILTER_SANITIZE_SPECIAL_CHARS); 
-    $filtro_busqueda= filter_input(INPUT_POST, 'filtro', FILTER_SANITIZE_SPECIAL_CHARS); 
-
-    //Habria que llamar a metodos de la clase Post que te devolvieran los mensajes asociados al usuario que has buscado 
-
-?> 
