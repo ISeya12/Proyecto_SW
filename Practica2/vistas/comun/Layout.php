6eeb7c2f926147a120237304439450ba939b607b
oscarFPG <95655329+oscarFPG@users.noreply.github.com>
2024-03-08 22:51:16 +0100

    Update Layout.php

--- Practica2/vistas/comun/Layout.php
+++ Practica2/vistas/comun/Layout.php
@@ -1,32 +1,34 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../css/estilos.css">
-<title> <?= $titulo_pagina ?> </title>
+    <link rel="stylesheet" href="css/estilos.css">
+    <title> Document </title>
 </head>
-
 <body>
- 
-
-    <?php 
-        require 'Header.php'; 
-        require 'Sidebar.php'; 
-    ?>    
-
-    <!-- Contenido de la app -->
-    <main>
-        <article>
-            <?= $contentPanel ?>
-        </article>
-    </main>
-
-    <?php 
-        require 'Footer.php';
-    ?> 
-
-
     
+    <div>
+        <div class="header">
+            <?php 
+                require_once 'Cabecera.php';
+            ?>
+        </div>
+
+        <div class="sidebar" style="display: inline-block; ">
+            <?php require_once 'Sidebar.php'; ?>
+        </div>
+            
+        <div class="main_content" style="display: inline-block;">
+            <main>
+                <?= $content ?>
+            </main>
+        </div>
+
+        <div class="footer">
+            <?php require_once 'Footer.php'; ?>
+        </div>
+    </div>
+
 </body>
 </html>
