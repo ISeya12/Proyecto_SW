678cb03f0254f90638fcf14027b701215abb4428
AlonsoCampi09 <alocam01@ucm.es>
2024-03-08 20:00:50 +0100

    Barra de busqueda implementada

--- Practica2/vistas/comun/Cabecera.php
+++ Practica2/vistas/comun/Cabecera.php
@@ -6,5 +6,20 @@ $contenido = mostrarSaludo();
 $header = <<<EOS
 $contenido
 EOS;
+?> 
 
-echo $contenido;
+<header> 
+
+   <?= $header?>   
+   
+   <!-- Implementacion barra de busqueda -->
+   <form  action= "./Login.php"  method= "post"> 
+        <p> Barra de busqueda </p> 
+        <input type= "search" value= " ">
+        <select name= "filtro"> 
+            <option value= "art"> Artista </option> 
+            <option value= "user"> Usuario </option> 
+            <option value= "ambos" selected> Ambos </option> 
+        </select> 
+    </form> 
+</header> 
--- Practica2/vistas/comun/Layout.php
+++ Practica2/vistas/comun/Layout.php
@@ -1,23 +1,32 @@
-<?php 
-    $content = "";
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Document </title>
+    <link rel="stylesheet" type="text/css" href="../css/estilos.css">
+<title> <?= $titulo_pagina ?> </title>
 </head>
+
 <body>
+ 
+
+    <?php 
+        require 'Header.php'; 
+        require 'Sidebar.php'; 
+    ?>    
+
+    <!-- Contenido de la app -->
+    <main>
+        <article>
+            <?= $contentPanel ?>
+        </article>
+    </main>
+
+    <?php 
+        require 'Footer.php';
+    ?> 
+
+
     
-    <?php require_once '../vistas/comun/Cabecera.php'; ?>
-
-    <?php require_once '../vistas/comun/Sidebar.php'; ?>
-
-    <div>
-        <?= $content ?>
-    </div>
-
 </body>
 </html>
