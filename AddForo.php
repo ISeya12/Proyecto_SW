c8a03f2d0dc0a01aac7d10362b31d43aff95f79e
oscar <oscarf1030p@gmail.com>
2024-03-10 16:07:10 +0100

    Crear post

--- AddForo.php
+++ AddForo.php
@@ -1,29 +1,22 @@
 <?php
 
-$servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
-$conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+require_once 'Config.php';
+require_once 'classes/Post.php';
+require_once 'classes/Usuario.php';
+
+$username = $_SESSION['username'];
+$post_text = $_POST['post_text'];
+$post_image = null;
+
+
+if(isset($_POST['post_image'])){
+    $post_image = $_POST['post_image'];
 }
 
-$titulo = $_POST['titulo'];
-$mensaje = $_POST['mensaje'];
-$imagen_nombre = $_FILES['imagen']['name'];
-$imagen_temp = $_FILES['imagen']['tmp_name'];
+$user = Usuario::buscaUsuario($username);
+$post = $user->publicarPost($post_text, $post_image);
 
+echo $post->generatePostHTML();
 
-$imagen_ruta = "carpeta_destino/" . $imagen_nombre;
-move_uploaded_file($imagen_temp, $imagen_ruta);
-
-$sql = "INSERT INTO tu_tabla (titulo, mensaje, imagen) VALUES ('$titulo', '$mensaje', '$imagen_ruta')";
-if ($conn->query($sql) === TRUE) {
-    echo "Datos insertados correctamente";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-$conn->close();
-?>
+// require_once 'Layout.php';
---
+++ CrearPost.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'Config.php';
+require_once 'CrearPostVista.php';
+
+$content = generatePostPublicationHTML();
+
+require_once 'Layout.php';
