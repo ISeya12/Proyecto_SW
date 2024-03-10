b2ac6cdf38c734e5a5af98507a00ba1295b1ba38
oscar <oscarf1030p@gmail.com>
2024-03-10 18:31:02 +0100

    Comprobacion de imagen en post

--- AddForo.php
+++ AddForo.php
@@ -5,13 +5,15 @@ require_once 'Config.php';
 require_once 'classes/Post.php';
 require_once 'classes/Usuario.php';
 
+$allowed = array( 'jpg', 'png' );
+
 $username = $_SESSION['username'];
 $post_text = $_POST['post_text'];
 $post_image = null;
 
-
-if(isset($_POST['post_image'])){
-    $post_image = $_POST['post_image'];
+if(isset($_FILES['post_image']) && checkFileExtension($_FILES['post_image']['name'], $allowed)){
+    echo 'Imagen Subida';
+    $post_image = $_FILES['post_image'];
 }
 
 $user = Usuario::buscaUsuario($username);
@@ -19,3 +21,9 @@ $post = $user->publicarPost($post_text, $post_image);
 $content = $post->generatePostHTML();
 
 require_once 'Layout.php';
+
+function checkFileExtension($file, $allowed){
+
+    $ext = pathinfo($file, PATHINFO_EXTENSION);
+    return in_array($ext, $allowed);
+}
--- CrearPostVista.php
+++ CrearPostVista.php
@@ -1,17 +1,18 @@
 <?php
 
 
+
 function generatePostPublicationHTML(){
 
     $html =<<<EOS
     <fieldset>
         <legend style="text-align: center; "><strong> Nueva Publicación </strong></legend>
-        <form name="datos_post" action="Addforo.php" method="post" enctype="multipart/form-data">
-            Mensaje: <textarea name="post_text" required style="resize: none; "></textarea><br><br>
-            Imagen: <input name="post_image" type="file" height="200" width="200">
-            <br><br>
-            Publicar <input type="submit">
-        </form>
+            <form name="datos_post" action="Addforo.php" method="post" enctype="multipart/form-data">
+                Mensaje: <textarea name="post_text" required style="resize: none; "></textarea><br><br>
+                Imagen: <input name="post_image" type="file" height="200" width="200">
+                <br><br>
+                Publicar <input type="submit">
+            </form>
     </fieldset>
     EOS;
 
