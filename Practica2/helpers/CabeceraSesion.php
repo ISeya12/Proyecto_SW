a1dab039ccc289c5b589eb15ba85de2f4413f565
IgnacioXr7 <ignacilu@ucm.es>
2024-03-21 10:51:18 +0100

    Cambios, cabecera

--- Practica2/helpers/CabeceraSesion.php
+++ Practica2/helpers/CabeceraSesion.php
@@ -13,15 +13,20 @@ function generateStaticHeader() {
         $loginImage = RUTA_IMG_PATH.'/FotoLogoutUser.png';
         $altText = 'Foto de logout';
         $link = RUTA_VISTAS_PATH.'/log/Logout.php';
-        $texto = "2Melody, una app para perder el tiempo escuchando música sin límites!"; 
+        $username = $_SESSION['username'];
+        $texto = "Bienvenido !!"; 
     }
     $html = <<<EOS
     <header class= 'header'>
         <p>
             <img src = '$iconImage' alt="Logo app" height="50" width="75">
-            $texto
+ 
         </p>
+        Barra de búsqueda
+        <p>
+        $texto
         <a href="$link"><img src="$loginImage" height="30" width="30" alt="$altText"></a>
+        </p>
     </header>
     EOS;
 
